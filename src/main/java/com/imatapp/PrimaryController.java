6a87c8b7960084dda097243d2fa1fa3aec2bd418
Ismail Sacic <cicas03@hotmail.com>
2023-05-22 13:14:41 +0200

    Added popup

--- src/main/java/com/imatapp/PrimaryController.java
+++ src/main/java/com/imatapp/PrimaryController.java
@@ -1,12 +1,17 @@
 package com.imatapp;
 
 
+import java.io.IOException;
+
 import com.imatapp.components.NavigationButton;
 import javafx.animation.FadeTransition;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.util.Duration;
 import se.chalmers.cse.dat216.project.IMatDataHandler;
@@ -15,7 +20,7 @@ public class PrimaryController  {
     private IMatDataHandler iMatDataHandler = IMatDataHandler.getInstance();
 
     @FXML
-    private Button packagesButton, allproductsButton, shoppingcartButton, accountButton;
+    private Button packagesButton, allproductsButton, shoppingcartButton, accountButton, popupButton;
 
     public NavigationButton[] navigationButtons;
 
@@ -26,12 +31,18 @@ public class PrimaryController  {
     private TextField searchbar;
 
     @FXML
-    private AnchorPane packagesPane, allproductsPane, shoppingcartPane, accountPane;
+    private GridPane popup;
+
+    @FXML
+    private BorderPane allContent;
+
+    @FXML
+    private AnchorPane packagesPane, allproductsPane, shoppingcartPane, accountPane, popupContent;
 
     private AnchorPane currentShowingPane;
 
     @FXML
-    public void initialize() {
+    public void initialize() throws IOException {
         // Set up current showing pane
         currentShowingPane = packagesPane;
 
@@ -41,9 +52,27 @@ public class PrimaryController  {
             new NavigationButton( shoppingcartPane,true, shoppingcartButton, this),
             new NavigationButton( accountPane, false, accountButton, this),
         };
-        
+     
+        popupButton.setOnAction( e -> {
+            hidePopup();
+        });
 
     }   
+    public void showPopup(AnchorPane content){
+        System.out.println("Showing popup");
+        popup.toFront();
+        popup.setVisible(true);
+        allContent.toBack();
+
+        popupContent.getChildren().clear();
+        popupContent.getChildren().add(content);
+    }
+
+    public void hidePopup(){
+        allContent.toFront();
+        popup.toBack();
+        popup.setVisible(false);
+    }
 
     public void switchPage( AnchorPane newPane){
         if (newPane == currentShowingPane){
