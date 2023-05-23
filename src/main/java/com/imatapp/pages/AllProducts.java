8f649a473e4026b651799b7b816ef0feca105167
Ismail Sacic <cicas03@hotmail.com>
2023-05-23 21:40:06 +0200

    some changes

--- src/main/java/com/imatapp/pages/AllProducts.java
+++ src/main/java/com/imatapp/pages/AllProducts.java
@@ -2,9 +2,22 @@ package com.imatapp.pages;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import se.chalmers.cse.dat216.project.IMatDataHandler;
+import se.chalmers.cse.dat216.project.ProductCategory;
 
 public class AllProducts {
+
+    IMatDataHandler iMatDataHandler = IMatDataHandler.getInstance();
+
     @FXML
-    
+    private Label berry, bread, cabbage, citrus_fruit, cold_drinks, dairies, exotic_fruit, fish, flour_sugar_salt, fruit, herb, hot_drinks, meat, melons, nuts_and_seeds, pasta, pod, potato_rice, root_vegetable, sweet, vegetable_fruit;
+
+
+    @FXML
+    public void initialize(){
+
+        
+
+    }
 
 }
