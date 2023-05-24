8224f23ba142694bc24a3dc0f3fc26e73465b06f
Ismail Sacic <cicas03@hotmail.com>
2023-05-24 14:33:36 +0200

    Some changtes

--- src/main/java/com/imatapp/pages/AllProducts.java
+++ src/main/java/com/imatapp/pages/AllProducts.java
@@ -1,24 +1,52 @@
 package com.imatapp.pages;
 
+import java.util.List;
+
+import com.imatapp.components.ProductCard;
+
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.text.Text;
 import se.chalmers.cse.dat216.project.IMatDataHandler;
+import se.chalmers.cse.dat216.project.Product;
+import se.chalmers.cse.dat216.project.ShoppingCart;
 
 public class AllProducts {
-
-
-    
 
     IMatDataHandler iMatDataHandler = IMatDataHandler.getInstance();
 
     @FXML
-    private Label berry, bread, cabbage, citrus_fruit, cold_drinks, dairies, exotic_fruit, fish, flour_sugar_salt, fruit, herb, hot_drinks, meat, melons, nuts_and_seeds, pasta, pod, potato_rice, root_vegetable, sweet, vegetable_fruit;
-    // how many labels are there? 21
+    private Text berryCategory, breadCategory, cabbageCategory, citrus_fruitCategory, cold_drinksCategory, dairiesCategory, exotic_fruitCategory, fishCategory, flour_sugar_saltCategory, fruitCategory, herbCategory, hot_drinksCategory, meatCategory, melonsCategory, nuts_and_seedsCategory, pastaCategory, podCategory, potato_riceCategory, root_vegetableCategory, sweetCategory, vegetable_fruitCategory;
+    @FXML
+    private FlowPane mainFlowPane;
 
     @FXML
     public void initialize(){
+        Text[] allCategories = new Text[] {
+            berryCategory, breadCategory, cabbageCategory, citrus_fruitCategory, cold_drinksCategory, dairiesCategory, exotic_fruitCategory, fishCategory, flour_sugar_saltCategory, fruitCategory, herbCategory, hot_drinksCategory, meatCategory, melonsCategory, nuts_and_seedsCategory, pastaCategory, podCategory, potato_riceCategory, root_vegetableCategory, sweetCategory, vegetable_fruitCategory
+        };
 
-        
+        List<Product> allProducts = iMatDataHandler.getProducts();
+        allProducts.forEach(product -> {
+            // print time benchmark
+            long startTime = System.nanoTime();
+            ProductCard productCard = new ProductCard(product, iMatDataHandler);
+            mainFlowPane.getChildren().add(productCard);
+            long endTime = System.nanoTime();
+            System.out.println("Time to create product card: " + (endTime - startTime)/ 1000000 + "ms");
+        });
+
+
+        for(Text category : allCategories ){
+    
+            //remove all the active classes
+            for (Text category2 : allCategories) {
+                category2.getStyleClass().remove("allproducts_categories_text_active");
+            }
+            category.getStyleClass().add("allproducts_categories_text_active");
+        }
+
+
 
     }
 
